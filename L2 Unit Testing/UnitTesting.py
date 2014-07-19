d15d60f55ab547f900d24ea5b2840bf5256d5410
Pavel Dvorkin <dpavel20@gmail.com>
2014-07-19 10:27:52 -0400

    Update UnitTesting.py

--- L2 Unit Testing/UnitTesting.py
+++ L2 Unit Testing/UnitTesting.py
@@ -1,16 +1,18 @@
-Here are your instructions:
+"""Here are your instructions:
 
-Make a UnitTesting_Homework project and assign it to the Python2_Homework working set. In this project, write a unittest test program for the following function.
+Make a UnitTesting_Homework project and assign it to the Python2_Homework working set. In this project, write a 
+unittest test program for the following function.
  (The test program makes unittest.TestCase assertions about the results of calling the function with known arguments.)
 
 def title(s):
     "How close is this function to str.title()?"
     return s[0].upper()+s[1:]Test your results for a given string s by comparing them with s.title().
 
- Because this is purely an exercise it's OK to put your test code in the same file as the function and just hand in a single file.
+ Because this is purely an exercise it's OK to put your test code in the same file as the function and 
+ just hand in a single file.
  Your file should be an importable module.
 
- You should be able to find an example that shows title(s) and s.title() diverge (have different output).
+ You should be able to find an example that shows title(s) and s.title() diverge (have different output)."""
 
 ###############################################################################################################################################################################################################################
 
