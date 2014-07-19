b0286d5d5ec5413db82a36dd17878397ee8d328b
Pavel Dvorkin <dpavel20@gmail.com>
2014-07-19 12:11:12 -0400

    Create settings.py

---
+++ L13 Email Search and Display/settings.py
@@ -0,0 +1,5 @@
+import datetime
+
+RECIPIENTS= [('Pavel','dpavel20@gmail.com'), ('Pavel2', 'pdvorkin@forham.edu')]
+STARTTIME = datetime.datetime.now() #datetime.datetime.now().strftime("%d %b %Y %H:%M:%S")
+DAYCOUNT =  10
