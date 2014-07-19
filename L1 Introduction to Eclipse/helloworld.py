0550881db24273780fdea6665edfa6a0d2df8873
Pavel Dvorkin <dpavel20@gmail.com>
2014-07-19 10:26:33 -0400

    Update helloworld.py

--- L1 Introduction to Eclipse/helloworld.py
+++ L1 Introduction to Eclipse/helloworld.py
@@ -1,11 +1,8 @@
-Here are your instructions:
+"""Here are your instructions:
 
-Create a IntroEclipse_Homework project and assign it to the Python2_Homework working set. In the new project, create a helloworld.py program that prints "Hello World." Hand in the finished program.
+Create a IntroEclipse_Homework project and assign it to the Python2_Homework working set. In the new project,
+create a helloworld.py program that prints "Hello World." Hand in the finished program."""
 
-Your Comment:
-no comment given
-Items Handed In
-Open Project Handed In
 
 #######################################################################################################################################################################################################################################################################################################################################
 
